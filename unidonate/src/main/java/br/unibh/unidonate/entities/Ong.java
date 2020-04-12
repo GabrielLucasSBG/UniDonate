383429183abfaaaee71b3ba072f2d92bdcf37535
RafaelSilva <rafaelfelps62@gmail.com>
2020-04-12 15:39:23 -0300

    ClassOng

---
+++ unidonate/src/main/java/br/unibh/unidonate/entities/Ong.java
@@ -0,0 +1,80 @@
+package br.unibh.unidonate.entities;
+
+import javax.persistence.*;
+
+@Entity
+public class Ong {
+	
+	public Ong() {}
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+	@Column(nullable = false)
+	private Long cnpj;
+
+	@Column(nullable = false)
+	private String endereco;
+	
+	@Column(nullable = false)
+	private Long telefone;
+	
+	@Column(nullable = false)
+	private String email;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public Long getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(Long telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public Long getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(Long cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	
+	
+	
+}
